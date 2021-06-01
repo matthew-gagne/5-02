adf421ae6d8e1e884050fbda56cd9fb1c525b864
matthewgagne1 <>
2021-06-01 14:40:22 +0000

    fix

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('button').addEventListener('click', Message);
-//press the button and it gives a alert that says hello 
+// press the button and it gives a alert that says hello
+
 function Message () {
   alert('hello')
 }
